827df50b4759c650ae493ac9676f578084f8d51a
jszafran <jszafran.pv@gmail.com>
2022-04-08 20:08:50 +0200

    Refactor client & add method for fetching headlines.

--- news_api_etl/newsapi.py
+++ news_api_etl/newsapi.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Iterable, List, Protocol
+from typing import Iterable, List, Optional, Protocol
 
 import requests
 
@@ -10,25 +10,49 @@ class NewsSource:
     name: str
 
 
+@dataclass
+class TopHeadline:
+    headline: str
+    source_id: str
+
+
 class NewsApiClient(Protocol):
-    def get_sources_for_language(self, language: str = "en") -> List[NewsSource]:
+    def get_sources(self, language: Optional[str] = None) -> List[NewsSource]:
         pass
 
-    def get_top_headlines_for_sources(self, sources: Iterable[NewsSource]):
+    def get_top_headlines(self, sources: Optional[Iterable[NewsSource]] = None):
         pass
 
 
 class NewsApiHTTPClient:
     def __init__(self, api_key: str, api_url: str = "https://newsapi.org/v2/") -> None:
         self._api_url = api_url
-        self._headers = {"X-Api-Key": api_key}
+        self._session = requests.Session()
+        self._session.headers.update({"X-Api-Key": api_key})
 
-    def get_sources_for_language(self, language: str = "en") -> List[NewsSource]:
-        response = requests.get(
-            self._api_url + f"sources?language={language}", headers=self._headers
-        )
+    def _get(self, url):
+        return self._session.get(self._api_url + url)
+
+    def get_sources(self, language: Optional[str] = None) -> List[NewsSource]:
+        endpoint_url = "sources"
+        if language is not None:
+            endpoint_url += f"?language={language}"
+        response = self._get(endpoint_url)
         response.raise_for_status()
         return [
             NewsSource(id=source["id"], name=source["name"])
             for source in response.json()["sources"]
+        ]
+
+    def get_top_headlines(self, sources: List[NewsSource]) -> List[TopHeadline]:
+        sources_ids = ",".join(s.id for s in sources)
+        endpoint_url = f"top-headlines/?sources={sources_ids}"
+        response = self._get(endpoint_url)
+        response.raise_for_status()
+        top_headlines = response.json()["articles"]
+        return [
+            TopHeadline(
+                headline=top_headline["title"], source_id=top_headline["source"]["id"]
+            )
+            for top_headline in top_headlines
         ]
